cf561cd1a5d03685f557d45001fddc80f6323802
abeel50 <abeelmuhammad50@gmail.com>
2023-01-13 00:29:24 +0500

    Redux- Flow

---
+++ src/redux/reducers/courseReducer.js
@@ -0,0 +1,13 @@
+import * as types from '../actions/actionTypes';
+
+export default function courseReducer(state = [], action) {
+    switch (action.type) {
+        case types.CREATE_COURSE:
+            //! state.push(action.course);  ! don't do this
+            return [...state, { ...action.course }];
+
+        default:
+            return state
+    }
+
+}
