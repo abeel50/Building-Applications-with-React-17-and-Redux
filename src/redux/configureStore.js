67a0c457fca943bb467b840cc352c5a564457098
abeel50 <abeelmuhammad50@gmail.com>
2023-02-22 23:23:54 +0500

    Course List Page, Author Names

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
   const composEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // * add support for Redux dev tools
@@ -8,6 +9,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    composEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+    composEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -5,7 +5,8 @@ export default function courseReducer(state = [], action) {
         case types.CREATE_COURSE:
             //! state.push(action.course);  ! don't do this
             return [...state, { ...action.course }];
-
+        case types.LOAD_COURSES_SUCCESS:
+            return action.courses;
         default:
             return state
     }
